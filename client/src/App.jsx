d017806e560d1dca571506125be7f7e721c3b392
KODIYARASU01 <kodiyarasu02gmail.com>
2024-04-24 13:33:34 +0530

    DigitalCard5 client side finished

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import NewCardDesign5 from './Component/NewCardDesign5';
+
+const App = () => {
+  return (
+    <div>
+ <NewCardDesign5/>
+    </div>
+  )
+}
+
+export default App;
